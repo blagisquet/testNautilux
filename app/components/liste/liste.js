f2a81324a59f2a497197606872c507b8202cbbe7
blagisquet <baptiste.lagisquet@imie.fr>
2020-03-02 10:36:35 +0100

    added simple logic

--- app/components/liste/liste.js
+++ app/components/liste/liste.js
@@ -9,5 +9,30 @@ angular.module('test.liste', ['ngRoute', 'ui.bootstrap'])
   });
 }])
 
-.controller('ListeCtrl', [function($scope) {
+.controller('ListeCtrl', ['$scope', 'http', function($scope, $http) {
+  $scope.now = new Date();
+  $scope.now.toISOString();
+
+    // GET ALL RECORD
+    $http({
+      method: 'post',
+      url: 'API/read.py',
+    }).then(function successCallback(response) {
+      $scope.intervention = response.data;
+    });
+
+    //ADD NEW RECORD
+    $scope.add = function () {
+      $http({
+        method: 'post',
+        url: 'API/create.py',
+        data: { title: $scope.title, description: $scope.description, localisation: $scope.localisation, name: $scope.name, date: $scope.date }
+      }).then(function successCallback(response) {
+        if (response.data.length > 0) {
+          $scope.users.push(response.data[0]);
+        } else {
+          console.log('Record not inserted');
+        }
+      });
+    }
 }])
